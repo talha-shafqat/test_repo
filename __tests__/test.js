47def0595e2b86a6f8dda614436bc132499b329d
talha-shafqat <talha.shafqat@leanix.net>
2021-07-19 12:23:41 +0200

    feat: add stuff

---
+++ __tests__/test.js
@@ -0,0 +1,7 @@
+describe("Testing with Jest", () => {
+    test("Addition", () => {
+      const sum = 2 + 3;
+      const expectedResult = 5;
+      expect(sum).toEqual(expectedResult);
+    });
+})
