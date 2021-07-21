79bd3b05bdeaa1052d55cb5018201b7d000c8dd8
talha-shafqat <talha.shafqat@leanix.net>
2021-07-21 09:53:34 +0200

    test

--- leanix/sendDataToWorkspace.sh
+++ leanix/sendDataToWorkspace.sh
@@ -1,22 +1,22 @@
 
 
-service_name=$(jq -r .name package.json)
-
+#service_name=$(jq -r .name package.json)
+service_name="test_name"
 export HOST=${MI_DEV_HOST:-eu.leanix.net}
 export TOKEN=${MI_DEV_TOKEN:-xxx}
 
 export SYNC_URL="https://${HOST}/services/integration-api/v1/synchronizationRuns"
-BEARER=$(curl -X POST --url https://${HOST}/services/mtm/v1/oauth2/token -u apitoken:${TOKEN} --data grant_type=client_credentials | jq -r '.access_token') 
-echo $BEARER
+TOKEN=$(curl -X POST --url https://${HOST}/services/mtm/v1/oauth2/token -u apitoken:${TOKEN} --data grant_type=client_credentials | jq -r '.access_token') 
 
 license-checker --json > $TRAVIS_BUILD_DIR/leanix/dependencies.json
 
 curl -X POST \
   -H 'Cache-Control: no-cache' \
-  -H "Authorization: Bearer ${BEARER}" \
+  -H "Authorization: Bearer ${TOKEN}" \
   -H 'Content-Type: multipart/form-data' \
   -F dependencies=@$TRAVIS_BUILD_DIR/leanix/dependencies.json \
-  -F manifest=@$TRAVIS_BUILD_DIR/lx-manifest.yaml \
+#  -F manifest=@$TRAVIS_BUILD_DIR/lx-manifest.yaml \
+  -F opMicroserviceId="${service_name}" \
   -F 'data={
   "version": "1.0.0",
   "stage": "dev",
